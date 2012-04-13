b2a6ab4dce466fe4e7fdbe7e5b29ab6890071b85
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-04-13 16:28:14 -0700

    added the ability to have environment sections. by default, if no RAILS_ENV or RACK_ENV is set, any enumerable hash value is excluded from the yaml file.  Values which are enumerable are mashed over the top level hash if the current env matches the top level key

--- lib/figaro/railtie.rb
+++ lib/figaro/railtie.rb
@@ -4,7 +4,37 @@ require "yaml"
 module Figaro
   class Railtie < ::Rails::Railtie
     config.before_configuration do
-      ENV.update(Figaro.env)
+      path = Rails.root.join("config/application.yml")
+      if File.exist?(path)
+        loader = Loader.new(path)
+        ENV.update(loader.to_hash)
+      end
+    end
+  end
+
+  class Loader
+    def initialize(path)
+      @config = YAML.load(File.read(path)) || {}
+    end
+
+    def to_hash
+      build_hash_from_enviroment
+    end
+
+    def enviroment
+      ENV['RAILS_ENV'] || ENV['RACK_ENV']
+    end
+
+    def build_hash_from_enviroment
+      @config.keys.each do |key|
+        if @config.fetch(key).respond_to?(:each)
+          values = @config.delete(key)
+          if key == enviroment
+            @config.update(values)
+          end
+        end
+      end
+      @config
     end
   end
 end
