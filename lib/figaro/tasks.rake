7c8690ad923c8620b019ac2a078e4b2f79f50ba9
Steve Richert <steve.richert@gmail.com>
2012-04-25 06:43:19 -0400

    Order Heroku configuration vars consistently across Ruby 1.8 and 1.9

--- lib/figaro/tasks.rake
+++ lib/figaro/tasks.rake
@@ -1,7 +1,7 @@
 namespace :figaro do
   desc "Configure Heroku according to application.yml"
   task :heroku, [:app] => :environment do |_, args|
-    vars = Figaro.env.map{|k,v| "#{k}=#{v}" }.join(" ")
+    vars = Figaro.env.map{|k,v| "#{k}=#{v}" }.sort.join(" ")
     command = "heroku config:add #{vars}"
     command << " --app #{args[:app]}" if args[:app]
     Kernel.system(command)
--- spec/figaro/tasks_spec.rb
+++ spec/figaro/tasks_spec.rb
@@ -4,13 +4,13 @@ describe "Figaro Rake tasks", :rake => true do
   describe "figaro:heroku" do
     it "configures Heroku" do
       Figaro.stub(:env => {"HELLO" => "world", "FOO" => "bar"})
-      Kernel.should_receive(:system).once.with("heroku config:add HELLO=world FOO=bar")
+      Kernel.should_receive(:system).once.with("heroku config:add FOO=bar HELLO=world")
       task.invoke
     end
 
     it "configures a specific Heroku app" do
       Figaro.stub(:env => {"HELLO" => "world", "FOO" => "bar"})
-      Kernel.should_receive(:system).once.with("heroku config:add HELLO=world FOO=bar --app my-app")
+      Kernel.should_receive(:system).once.with("heroku config:add FOO=bar HELLO=world --app my-app")
       task.invoke("my-app")
     end
   end
