7463b18781ba087a56478bd83baa11535876d33c
Srushti Ambekallu <srushti@ambekallu.com>
2012-09-14 17:29:13 +0530

    Upcasing all calls, fixed respond_to? & raising errors if the setting is not found

--- lib/figaro/figaro_settings.rb
+++ lib/figaro/figaro_settings.rb
@@ -1,8 +1,18 @@
 class FigaroSettings
+  class SettingNotFoundError < StandardError; end
+
   class << self
     def method_missing(symbol, *args, &block)
-      self.class.send(:define_method, symbol) { ENV[symbol.to_s] }
-      send(symbol)
+      if ENV.key?(symbol.to_s.upcase)
+        self.class.send(:define_method, symbol) { ENV[symbol.to_s.upcase] }
+        send(symbol)
+      else
+        raise SettingNotFoundError, symbol
+      end
+    end
+
+    def respond_to?(symbol)
+      ENV.key?(symbol.to_s.upcase)
     end
   end
 end
--- spec/figaro/figaro_settings_spec.rb
+++ spec/figaro/figaro_settings_spec.rb
@@ -2,7 +2,24 @@ require "spec_helper"
 
 describe FigaroSettings do
   it "retrieves values from the environment" do
-    ENV['some_key'] = 'some value'
+    ENV['SOME_KEY'] = 'some value'
+    FigaroSettings.SOME_KEY.should == 'some value'
+  end
+
+  it "retrieves the value of the upcased version of the key" do
+    ENV['SOME_KEY'] = 'some value'
     FigaroSettings.some_key.should == 'some value'
+  end
+
+  it "responds to any method where an environment variable of the same name exists" do
+    FigaroSettings.respond_to?(:another_key).should be_false
+    ENV['ANOTHER_KEY'] = 'another value'
+    FigaroSettings.respond_to?(:another_key).should be_true
+  end
+
+  it "raises an error if it can't find a key" do
+    expect do
+      FigaroSettings.yet_another_key
+    end.to raise_error(FigaroSettings::SettingNotFoundError)
   end
 end
