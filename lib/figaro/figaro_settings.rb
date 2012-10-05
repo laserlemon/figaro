54f058b77a711834d942e41aca620a85a5e7f7c4
Pankaj Tyagi <ptyagi@redmica.com>
2012-10-05 15:01:27 -0400

    refactored some code
    refactored code

--- lib/figaro/figaro_settings.rb
+++ lib/figaro/figaro_settings.rb
@@ -8,13 +8,7 @@ class FigaroSettings
 
   class << self
     def method_missing(symbol, *args, &block)
-      val = nil
-      if RUBY_ENGINE == "jruby"
-        val = java.lang.System.get_property(symbol.to_s)
-      end
-      if val.nil? && ENV.key?(symbol.to_s.upcase)
-        val = ENV[symbol.to_s.upcase]
-      end
+      val = get_val(symbol)
       unless val.nil?
         self.class.send(:define_method, symbol) { val }
         send(symbol)
@@ -24,13 +18,23 @@ class FigaroSettings
     end
 
     def respond_to?(symbol)
+      return !get_val(symbol).nil?
+    end
+    
+    private
+    
+    def get_val(symbol)
       val = nil
       if RUBY_ENGINE == "jruby"
         val = java.lang.System.get_property(symbol.to_s)
       end
+      if val.nil? && ENV.key?(symbol.to_s)
+        val = ENV[symbol.to_s]
+      end
       if val.nil? && ENV.key?(symbol.to_s.upcase)
         val = ENV[symbol.to_s.upcase]
       end
+      val
     end
   end
 end
