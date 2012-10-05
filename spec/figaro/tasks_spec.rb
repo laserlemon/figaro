0b8cff0ab1cf788249dae95df38b26bb9419e3f2
Pankaj Tyagi <ptyagi@redmica.com>
2012-10-05 16:12:08 -0400

    Fix some cucumber issue
    Travis build cucumber test fail but pass with rspec

--- spec/figaro/tasks_spec.rb
+++ spec/figaro/tasks_spec.rb
@@ -4,36 +4,41 @@ describe "Figaro Rake tasks", :rake => true do
   def common_test(expect_cmd, app=nil)
     Figaro.stub(:env => {"HELLO" => "world", "FOO" => "bar"})
     Kernel.should_receive(:system).once.with(expect_cmd)
-    task.invoke(app)
   end
 
   describe "figaro:heroku" do
     it "configures Heroku" do
       common_test "heroku config:add FOO=bar HELLO=world"
+      task.invoke
     end
 
     it "configures a specific Heroku app" do
       common_test "heroku config:add FOO=bar HELLO=world --app my-app", "my-app"
+      task.invoke("my-app")
     end
   end
 
   describe "figaro:cloudbees" do
     it "configures Cloudbees" do
       common_test "bees config:set FOO=bar HELLO=world"
+      task.invoke
     end
 
     it "configures a specific Cloudbees app" do
       common_test "bees config:set FOO=bar HELLO=world -a my-app", "my-app"
+      task.invoke("my-app")
     end
   end
   
   describe "figaro:heroku_test" do
     it "echoes commands to configure Heroku" do
       common_test "echo heroku config:add FOO=bar HELLO=world"
+      task.invoke
     end
 
     it "echoes commands to configure a specific Heroku app" do
       common_test "echo heroku config:add FOO=bar HELLO=world --app my-app", "my-app"
+      task.invoke("my-app")
     end
   end
 
@@ -41,10 +46,12 @@ describe "Figaro Rake tasks", :rake => true do
   describe "figaro:cloudbees_test" do
     it "echoes commands to configure Cloudbees" do
       common_test "echo bees config:set FOO=bar HELLO=world"
+      task.invoke
     end
   
     it "echoes commands to configure Cloudbees app" do
       common_test "echo bees config:set FOO=bar HELLO=world -a my-app", "my-app"
+      task.invoke("my-app")
     end
   end
 
