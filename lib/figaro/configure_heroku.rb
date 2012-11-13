737eb0c0bd955be1be35b798fe694d83ba981f7d
Justin Spradlin <jspradlin@gmail.com>
2012-11-13 07:10:12 -0500

    Removing unecessary comment.

--- lib/figaro/configure_heroku.rb
+++ lib/figaro/configure_heroku.rb
@@ -1,9 +1,6 @@
 module Figaro
   class ConfigureHeroku
     def execute(args = {})
-
-      puts "I'm being executed!!!"
-
       app = args[:app] ? " --app #{args[:app]}" : ""
       rails_env = `heroku config:get RAILS_ENV#{app}`
       Rails.env = rails_env if rails_env.present?
