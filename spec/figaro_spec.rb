913a10e1312f97657048b389514f0df65ce31895
Steve Richert <steve.richert@gmail.com>
2013-01-07 10:36:21 -0500

    Refactor the Figaro.vars and Figaro.env specs
    Still need coverage on .raw, .yaml, .path and .environment.

--- spec/figaro_spec.rb
+++ spec/figaro_spec.rb
@@ -3,29 +3,43 @@ require "spec_helper"
 describe Figaro do
   describe ".vars" do
     it "dumps and sorts env" do
-      Figaro.stub(:raw => {"HELLO" => "world", "FOO" => "bar"})
+      Figaro.stub(:env => Figaro::Env.from("HELLO" => "world", "FOO" => "bar"))
+
       Figaro.vars.should == "FOO=bar HELLO=world"
     end
 
     it "allows access to a particular environment" do
-      Figaro.stub(:raw => {"development" => {"HELLO" => "developers"}, "production" => {"HELLO" => "world"}})
-      Figaro.vars(:development).should == "HELLO=developers"
-      Figaro.vars(:production).should == "HELLO=world"
+      Figaro.stub(:env).with("development")
+        .and_return(Figaro::Env.from("HELLO" => "developers"))
+      Figaro.stub(:env).with("production")
+        .and_return(Figaro::Env.from("HELLO" => "world"))
+
+      Figaro.vars("development").should == "HELLO=developers"
+      Figaro.vars("production").should == "HELLO=world"
     end
   end
 
   describe ".env" do
-    it "makes ENV values accessible" do
-      ENV["HELLO"] = "world"
-      Figaro.stub(:raw => {})
-      Figaro.env.hello.should == "world"
-      ENV.delete("HELLO")
+    it "is a Figaro env instance" do
+      Figaro.stub(:raw => {"FOO" => "bar"})
+
+      Figaro.env.should be_a(Figaro::Env)
     end
 
     it "allows access to a particular environment" do
-      Figaro.stub(:raw => {"development" => {"HELLO" => "developers"}, "production" => {"HELLO" => "world"}})
+      Figaro.stub(:raw).and_return(
+        "development" => {"HELLO" => "developers"},
+        "production" => {"HELLO" => "world"}
+      )
+
       Figaro.env(:development).should == {"HELLO" => "developers"}
       Figaro.env(:production).should == {"HELLO" => "world"}
+    end
+
+    it "stringifies keys and values" do
+      Figaro.stub(:raw).and_return(:LUFTBALLOONS => 99)
+
+      Figaro.env.should == {"LUFTBALLOONS" => "99"}
     end
   end
 end
