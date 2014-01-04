8b07d19b00fd86dfaa8cefc04bdb78aa15b026a8
Steve Richert <steve.richert@gmail.com>
2014-01-04 10:24:02 -0500

    Stop bundle executing every Rails test command

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Figaro::Rails do
   before do
     run_simple(<<-CMD)
-      bundle exec rails new example \
+      rails new example \
         --skip-gemfile \
         --skip-bundle \
         --skip-keeps \
@@ -14,18 +14,12 @@ describe Figaro::Rails do
         --quiet
       CMD
     cd("example")
-    write_file("Gemfile", <<-EOF)
-gem "rails"
-gem "sqlite3"
-gem "figaro", path: "#{ROOT}"
-EOF
-    run_simple("bundle install")
   end
 
   describe "initialization" do
     it "loads application.yml" do
       write_file("config/application.yml", "HELLO: world")
-      run_simple("bundle exec rails runner 'puts Figaro.env.hello'")
+      run_simple("rails runner 'puts Figaro.env.hello'")
 
       assert_partial_output("world", all_stdout)
     end
@@ -37,7 +31,7 @@ development:
   database: db/<%= ENV["FOO"] %>.sqlite3
 EOF
       write_file("config/application.yml", "FOO: bar")
-      run_simple("bundle exec rake db:migrate")
+      run_simple("rake db:migrate")
 
       check_file_presence(["db/bar.sqlite3"], true)
     end
@@ -45,13 +39,13 @@ EOF
 
   describe "rails generate figaro:install" do
     it "generates application.yml" do
-      run_simple("bundle exec rails generate figaro:install")
+      run_simple("rails generate figaro:install")
 
       check_file_presence(["config/application.yml"], true)
     end
 
     it "ignores application.yml" do
-      run_simple("bundle exec rails generate figaro:install")
+      run_simple("rails generate figaro:install")
 
       check_file_content(".gitignore", %r(^/config/application\.yml$), true)
     end
@@ -59,7 +53,7 @@ EOF
 
   describe "rake figaro:heroku" do
     it "is included" do
-      run_simple("bundle exec rake --tasks figaro:heroku")
+      run_simple("rake --tasks figaro:heroku")
 
       assert_partial_output("rake figaro:heroku[app]  # Configure Heroku according to application.yml", all_stdout)
     end
