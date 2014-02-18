d11ca136890b9d2515aa47cd167c17231b7cfd99
Ryan Nielson <ryan@toplog.ca>
2014-02-17 20:58:30 -0400

    Made Figaro load configuration earlier so it can be used in places like database.yml.

--- lib/figaro/rails/railtie.rb
+++ lib/figaro/rails/railtie.rb
@@ -1,7 +1,7 @@
 module Figaro
   module Rails
     class Railtie < ::Rails::Railtie
-      initializer "figaro.load", before: :load_environment_config do
+      config.before_configuration do
         Figaro.load
       end
     end
