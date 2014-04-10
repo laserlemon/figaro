laserlemon/figaro

commit 3aa986023f9586d02033ee0b3e6478260a9911d6

Attempt to properly set Heroku ENV on Windows

lib/figaro/cli/heroku_set.rb
@@ -18,7 +18,11 @@ module Figaro
       end
 
       def vars
-        configuration.keys.map { |k| %(#{k}="$#{k}") }.join(" ")
+        configuration.keys.map { |k| var(k) }.join(" ")
+      end
+
+      def var(key)
+        Gem.win_platform? ? "#{key}=%#{key}%" : "#{key}=$#{key}"
       end
     end
   end

commit 6622c2fc5e43ce13281ae82b280fec6e0d720e53

Heroku config:set was broken by commit 3aa986023f9586d02033ee0b3e6478260a9911d6

lib/figaro/cli/heroku_set.rb
@@ -22,7 +22,7 @@ module Figaro
       end
 
       def var(key)
-        Gem.win_platform? ? "#{key}=%#{key}%" : "#{key}=$#{key}"
+        Gem.win_platform? ? "#{key}=%#{key}%" : %(#{key}="$#{key}")
       end
     end
   end