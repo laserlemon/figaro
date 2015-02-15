09f67725e369d0e618078d15cb9f6cf135991383
Jin Hian Lee <jinhian.lee@evie.ai>
2015-02-15 18:14:42 +0000

    Initial support for Dokku PaaS (https://github.com/progrium/dokku)
    Usage:
    figaro dokku:set --server=<example-dokku-server.com> --app=<example-app>
    
    Limitations:
    1. Requires Dokku server and app to be specified via --app and --server
    2. Requires Dokku server to be accessible via ssh from the client machine for the 'dokku' user
    
    Future enhancements could include supporting arbitrary users (which would require dynamically changing the command) and potentially reading the app and server values from the client/shell ENV

--- lib/figaro/cli.rb
+++ lib/figaro/cli.rb
@@ -37,5 +37,39 @@ module Figaro
       require "figaro/cli/heroku_set"
       HerokuSet.run(options)
     end
+
+    # figaro dokku:set
+
+    desc "dokku:set --app=<app> --server=<server>", "Send Figaro configuration to a Dokku app."
+    long_desc <<-LONGDESC
+      Sends the Figaro configuration to a Dokku app named <app> on Dokku server <server>.
+
+      Since Dokku does not provide a client, this command requires that:
+      1. the local machine has ssh access to the Dokku server for the 'dokku' user
+      2. the Dokku server  (e.g. dokku-example.com)
+      3. the app name
+    LONGDESC
+
+    method_option "app",
+      required: true, 
+      aliases: ["-a"],
+      desc: "Specify a Dokku app"
+    method_option "environment",
+      aliases: ["-e"],
+      desc: "Specify an application environment"
+    method_option "path",
+      aliases: ["-p"],
+      default: "config/application.yml",
+      desc: "Specify a configuration file path"
+    method_option "server",
+      required: true,
+      aliases: ["-s"],
+      desc: "Specify a Dokku server (dokku-example.com)"
+ 
+    define_method "dokku:set" do
+      require "figaro/cli/dokku_set"
+      DokkuSet.run(options)
+    end
+
   end
 end
