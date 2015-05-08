b26a8130518b9d0ea61885b0387eae54f0a12e59
Faraz Yashar <faraz.yashar@gmail.com>
2015-05-08 19:45:04 -0400

    Add command for Elastic Beanstalk Support

--- lib/figaro/cli.rb
+++ lib/figaro/cli.rb
@@ -37,5 +37,36 @@ module Figaro
       require "figaro/cli/heroku_set"
       HerokuSet.run(options)
     end
+
+    # figaro eb:set
+
+    desc "eb:set", "Send Figaro configuration to Elastic Beanstalk"
+
+    method_option "eb-env",
+      desc: "Specify the Elastic Beanstalk environment to target"
+    method_option "environment",
+      aliases: ["-e"],
+      desc: "Specify an application environment"
+    method_option "path",
+      aliases: ["-p"],
+      default: "config/application.yml",
+      desc: "Specify a configuration file path"
+    method_option "profile",
+      desc: "Use a specific profile from your credential file"
+    method_option "verify-ssl",
+      type: :boolean,
+      desc: "Whether to verify AWS SSL certificates",
+      default: true
+
+    method_option "region",
+      aliases: ["-r"],
+      desc: "Use a specific region"
+    method_option "timeout",
+      desc: "The number of minutes before the command times out"
+
+    define_method "eb:set" do
+      require "figaro/cli/eb_set"
+      EBSet.run(options)
+    end
   end
 end
