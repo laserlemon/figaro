6a04365272889475e7efeea1266d9053f0ecb2fa
Faraz Yashar <faraz.yashar@gmail.com>
2015-05-08 20:23:47 -0400

    Update old specs to use expect_ran helper

--- spec/figaro/cli/heroku_set_spec.rb
+++ spec/figaro/cli/heroku_set_spec.rb
@@ -7,20 +7,14 @@ describe "figaro heroku:set" do
 
   it "sends Figaro configuration to Heroku" do
     run_simple("figaro heroku:set")
-
-    command = commands.last
-    expect(command.name).to eq("heroku")
-    expect(command.args).to eq(["config:set", "foo=bar"])
+    expect_ran("heroku", "config:set", "foo=bar")
   end
 
   it "respects path" do
     write_file("env.yml", "foo: bar")
 
     run_simple("figaro heroku:set -p env.yml")
-
-    command = commands.last
-    expect(command.name).to eq("heroku")
-    expect(command.args).to eq(["config:set", "foo=bar"])
+    expect_ran("heroku", "config:set", "foo=bar")
   end
 
   it "respects environment" do
@@ -31,37 +25,23 @@ test:
 EOF
 
     run_simple("figaro heroku:set -e test")
-
-    command = commands.last
-    expect(command.name).to eq("heroku")
-    expect(command.args).to eq(["config:set", "foo=baz"])
+    expect_ran("heroku", "config:set", "foo=baz")
   end
 
   it "targets a specific Heroku app" do
     run_simple("figaro heroku:set -a foo-bar-app")
-
-    command = commands.last
-    expect(command.name).to eq("heroku")
-    expect(command.args.shift).to eq("config:set")
-    expect(command.args).to match_array(["foo=bar", "--app=foo-bar-app"])
+    expect_ran("heroku", "config:set", "foo=bar", "--app=foo-bar-app")
   end
 
   it "targets a specific Heroku git remote" do
     run_simple("figaro heroku:set --remote production")
-
-    command = commands.last
-    expect(command.name).to eq("heroku")
-    expect(command.args.shift).to eq("config:set")
-    expect(command.args).to match_array(["foo=bar", "--remote=production"])
+    expect_ran("heroku", "config:set", "foo=bar", "--remote=production")
   end
 
   it "handles values with special characters" do
     overwrite_file("config/application.yml", "foo: bar baz")
 
     run_simple("figaro heroku:set")
-
-    command = commands.last
-    expect(command.name).to eq("heroku")
-    expect(command.args).to eq(["config:set", "foo=bar baz"])
+    expect_ran("heroku", "config:set", "foo=bar baz")
   end
 end
