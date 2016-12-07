7a43689dd7622f1f4fada54944e096d1ccf0731c
Steve Richert <steve.richert@gmail.com>
2016-12-06 22:36:34 -0500

    Refactor ENV reset after each spec

--- spec/support/reset.rb
+++ spec/support/reset.rb
@@ -1,12 +1,11 @@
 RSpec.configure do |config|
   config.around do |example|
-    original_env_keys = ENV.keys
-
-    example.run
-
-    new_env_keys = ENV.keys - original_env_keys
-    new_env_keys.each { |key| ENV[key] = nil }
-
-    Thread.current[:figaro_config] = nil
+    begin
+      original_env = ENV.to_hash
+      example.run
+    ensure
+      ENV.replace(original_env)
+      Thread.current[:figaro_config] = nil
+    end
   end
 end
