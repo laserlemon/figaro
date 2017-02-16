6461150ca09de70d1df97b987608532bf5bf2d8b
Harry Lascelles <github@hlascelles.com>
2017-02-16 11:53:31 +0000

    Using a file path expansion compatible with ruby 1.9

--- spec/internal/config/initializers/initialize_figaro.rb
+++ spec/internal/config/initializers/initialize_figaro.rb
@@ -2,6 +2,6 @@ require 'figaro'
 
 Figaro.application = Figaro::Application.new(
   environment: ::Rails.env,
-  path: File.expand_path('../application.yml', __dir__)
+  path: File.expand_path('../application.yml', File.dirname(__FILE__))
 )
 Figaro.load
