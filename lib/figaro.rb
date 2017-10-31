42a1ecdf84502779c5b529def46fab59a1de4425
Steve Richert <steve.richert@gmail.com>
2017-10-31 13:33:57 -0400

    Add the Figaro.load, Figaro.loaded?, and Figaro.reload methods

--- lib/figaro.rb
+++ lib/figaro.rb
@@ -6,4 +6,17 @@ module Figaro
   def self.config
     ::Thread.current[:figaro_config] ||= Figaro::Config.load
   end
+
+  def self.load
+    config
+    true
+  end
+
+  def self.loaded?
+    !!::Thread.current[:figaro_config]
+  end
+
+  def self.reload
+    ::Thread.current[:figaro_config] = Figaro::Config.load
+  end
 end
