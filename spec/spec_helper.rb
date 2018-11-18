c20d595ec23e646a6a36fa21b16968197ee8e03e
Steve Richert <steve.richert@gmail.com>
2018-11-18 16:20:36 -0500

    Auto-correct the Style/ExpandPathArguments cop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,4 @@ require "figaro"
 
 Bundler.require(:test)
 
-Dir[File.expand_path("../support/*.rb", __FILE__)].each { |f| require f }
+Dir[File.expand_path('support/*.rb', __dir__)].each { |f| require f }
