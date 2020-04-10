97e9dfcfe4488a9ca35e46688747932ad5156265
Steve Richert <steve.richert@gmail.com>
2020-04-10 19:47:26 -0400

    Fix a typo

--- lib/figaro/error.rb
+++ lib/figaro/error.rb
@@ -7,7 +7,7 @@ module Figaro
   class TypeLoadError < Figaro::Error
     def initialize(type:, value:)
       message = "The value #{value.inspect} cannot be loaded by a variable " \
-        "of type of type #{type.class.name}."
+        "of type #{type.class.name}."
 
       super(message)
     end
